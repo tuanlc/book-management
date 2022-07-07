adef6defbf3a3ef9ca49edea5d3a5425023dc6cb
Tuan LE CONG <tuancnttbk93@gmail.com>
2022-07-07 08:02:29 +0700

    Make database dns configurable

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -10,8 +12,12 @@ var (
 )
 
 func Connect() {
-	uri := "host=localhost user=admin password=admin dbname=book-management port=5432 sslmode=disable TimeZone=Asia/Ho_Chi_Minh"
-	db, err := gorm.Open(postgres.Open(uri), &gorm.Config{})
+	databaseDNS := os.Getenv("DATABASE_DNS")
+
+	if databaseDNS == "" {
+		databaseDNS = "host=localhost user=admin password=admin dbname=book-management port=5432 sslmode=disable TimeZone=Asia/Ho_Chi_Minh"
+	}
+	db, err := gorm.Open(postgres.Open(databaseDNS), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
